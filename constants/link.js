db0d4ad7737ea3571eddac508b4d4c65ae79496c
ProtoSG <diegoalbertosalazar177@gmail.com>
2025-04-10 15:13:51 -0500

    feat: add constans

---
+++ constants/link.js
@@ -0,0 +1,29 @@
+import { DashboardIcon } from "../icons/index.js";
+
+export const links = [
+  {
+    link: "#/dashboard",
+    name: "Dashboard",
+    icon: DashboardIcon(),
+  },
+  {
+    link: "#/pacientes",
+    name: "Pacientes",
+    icon: DashboardIcon(),
+  },
+  {
+    link: "#/historia-clinica",
+    name: "Historia Clinica",
+    icon: DashboardIcon(),
+  },
+  {
+    link: "#/citas-medicas",
+    name: "Citas Médicas",
+    icon: DashboardIcon(),
+  },
+  {
+    link: "#/recetas",
+    name: "Recetas",
+    icon: DashboardIcon(),
+  },
+];
